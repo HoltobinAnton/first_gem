195aef1da17efb99864ae1299185fa7eab6b2330
YogurtYoung <antoshkabest@ua.fm>
2016-11-02 09:14:03 +0200

    finish)

--- lib/first_gem_holtobin_anton.rb
+++ lib/first_gem_holtobin_anton.rb
@@ -3,7 +3,10 @@ require "first_gem_holtobin_anton/version"
 module FirstGemHoltobinAnton
   puts 'Hello Anton'
 
-  define_method :+ do |other|
-    self.to_s + other.to_s
+  class Fixnum
+    define_method :+ do |other|
+      self.to_s + other.to_s
+    end
   end
 end
+
