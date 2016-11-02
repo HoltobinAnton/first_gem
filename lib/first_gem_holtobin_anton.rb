0319e95b6d0437371138ed94fc07b2d0a1f1180f
YogurtYoung <antoshkabest@ua.fm>
2016-11-02 09:16:22 +0200

    example

--- lib/first_gem_holtobin_anton.rb
+++ lib/first_gem_holtobin_anton.rb
@@ -1,12 +1,8 @@
 require "first_gem_holtobin_anton/version"
 
 module FirstGemHoltobinAnton
-  puts 'Hello Anton'
+  5.times { puts 'Hello Anton' }
 
-  class Fixnum
-    define_method :+ do |other|
-      self.to_s + other.to_s
-    end
-  end
+
 end
 
